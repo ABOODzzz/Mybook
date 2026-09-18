a437d3d25ae0993d1d762bdfe0adb63155a26b23
ABOODzzz[200~$ git config --global user.name ABOODzzz <A.Aljamal2@gju.edu.jo.com~git>
2026-09-18 22:29:55 +0300

    first_point

---
+++ login.php
@@ -0,0 +1,38 @@
+<?php
+
+?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MyBook</title>
+    <style>
+        #Mybook{
+            height: 100px;
+            background-color: rgb(59, 89,152);
+            color: #d9dfeb;
+            font-size: 40px;
+        }
+        #SignUp{
+            width: 60px;
+            background-color: #42b72a;
+            color: #d9dfeb;
+            text-align: center;
+            padding: 2px;
+            border-radius: 4px;
+            float: right;
+        }
+
+    </style>
+</head>
+<body style="font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;background-color: #e9ebee;">
+<div id="Mybook">
+    Mybook
+</div>
+
+<div id="SignUp">
+    SignUp
+</div>
+</body>
+</html>
